85f5ab73451a134db9a47f2ecec44de9e187c379
LiuWen <162258246+houli123@users.noreply.github.com>
2025-04-16 13:52:05 +0800

    study proxies

---
+++ Web Spider/MyProject/004proxies.py
@@ -0,0 +1,9 @@
+import requests
+
+proxies={
+    'https':'106.12.111.83:9103'  # 代理IP和端口
+}
+
+res=requests.get('https://www.baidu.com',proxies=proxies)
+res.encoding=res.apparent_encoding
+print(res.text)
